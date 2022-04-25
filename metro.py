bb22dc6d7e086b980f7dd992133035eb1219c777
Thomas González Saito <71399006+ThGoSa2003@users.noreply.github.com>
2022-04-25 20:02:49 +0200

    Afegir csv

--- metro.py
+++ metro.py
@@ -1,5 +1,14 @@
 import networkx
 import staticmap
+import pandas as pd
+
+url = 'https://raw.githubusercontent.com/jordi-petit/ap2-metro-nyam-2022/main/data/restaurants.csv'
+csv_restaurants = pd.read_csv(url)
+url = 'https://raw.githubusercontent.com/jordi-petit/ap2-metro-nyam-2022/main/data/blob/main/estacions.csv'
+csv_restaurants = pd.read_csv(url)
+url = 'https://raw.githubusercontent.com/jordi-petit/ap2-metro-nyam-2022/main/data/blob/main/accessos.csv'
+csv_restaurants = pd.read_csv(url)
+
 
 def get_metro_graph() -> MetroGraph: ...
 
