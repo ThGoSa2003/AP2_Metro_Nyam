665a230dbca082c07a5cc8dde346c661f36212be
Thomas González Saito <71399006+ThGoSa2003@users.noreply.github.com>
2022-04-25 20:04:49 +0200

    Afegir csv

--- restaurants.py
+++ restaurants.py
@@ -1,5 +1,15 @@
 import pandas
+import pandas as pd
 import fuzzysearch
+
+
+url = 'https://raw.githubusercontent.com/jordi-petit/ap2-metro-nyam-2022/main/data/restaurants.csv'
+csv_restaurants = pd.read_csv(url)
+url = 'https://raw.githubusercontent.com/jordi-petit/ap2-metro-nyam-2022/main/data/blob/main/estacions.csv'
+csv_restaurants = pd.read_csv(url)
+url = 'https://raw.githubusercontent.com/jordi-petit/ap2-metro-nyam-2022/main/data/blob/main/accessos.csv'
+csv_restaurants = pd.read_csv(url)
+
 
 @dataclass
 class Restaurant: ...
