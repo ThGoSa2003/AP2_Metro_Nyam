57f2e541376fa5d81a65347c5092af7e88bede9b
ThGoSa2003 <tgsml2003@gmail.com>
2022-04-28 14:27:18 +0200

    Style

--- restaurants.py
+++ restaurants.py
@@ -1,11 +1,6 @@
 import pandas as pd
 import fuzzysearch
 
-
-url = 'https://raw.githubusercontent.com/jordi-petit/ap2-metro-nyam-2022/main/\
-data/restaurants.csv'
-csv_restaurants = pd.read_csv(url)
-
 @dataclass
 class Restaurant:
     """
@@ -54,6 +49,8 @@ Restaurants = List[Restaurant]
 
 
 def read() -> Restaurants:
+    csv_restaurants = pd.read_csv('https://raw.githubusercontent.com/\
+    jordi-petit/ap2-metro-nyam-2022/main/data/restaurants.csv')
     dim = csv_restaurants.shape()
     for i in range(dim[0]):
 
