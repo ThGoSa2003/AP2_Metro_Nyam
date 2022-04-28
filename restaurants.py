74afa32a226be955caa4ce3635c5c651abaf0106
orionLP <oriol.lopez.petit@estudiantat.upc.edu>
2022-04-28 13:59:13 +0200

    erased links in restaurants.py

--- restaurants.py
+++ restaurants.py
@@ -4,19 +4,16 @@ import fuzzysearch
 
 url = 'https://raw.githubusercontent.com/jordi-petit/ap2-metro-nyam-2022/main/data/restaurants.csv'
 csv_restaurants = pd.read_csv(url)
-url = 'https://raw.githubusercontent.com/jordi-petit/ap2-metro-nyam-2022/main/data/blob/main/estacions.csv'
-csv_restaurants = pd.read_csv(url)
-url = 'https://raw.githubusercontent.com/jordi-petit/ap2-metro-nyam-2022/main/data/blob/main/accessos.csv'
-csv_restaurants = pd.read_csv(url)
-
 
 @dataclass
-class Restaurant: ...
+class Restaurant:
 
 Restaurants = List[Restaurant]
 
 
-def read() -> Restaurants: ...
+def read() -> Restaurants:
+    dim = csv_restaurants.shape()
+    for i in range(dim[0]):
 
 
 def find(query: str, restaurants: Restaurants) -> Restaurants: ...
