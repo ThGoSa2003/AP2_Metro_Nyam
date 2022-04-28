c8d0b4e376ae5b7515491de3155b7721fa698c72
ThGoSa2003 <tgsml2003@gmail.com>
2022-04-28 14:09:06 +0200

    Style

--- restaurants.py
+++ restaurants.py
@@ -2,7 +2,8 @@ import pandas as pd
 import fuzzysearch
 
 
-url = 'https://raw.githubusercontent.com/jordi-petit/ap2-metro-nyam-2022/main/data/restaurants.csv'
+url = 'https://raw.githubusercontent.com/jordi-petit/ap2-metro-nyam-2022/main/\
+data/restaurants.csv'
 csv_restaurants = pd.read_csv(url)
 
 @dataclass
