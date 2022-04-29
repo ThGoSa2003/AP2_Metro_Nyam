f08a12e4903ffda5bef74e96b29043f81559d07b
ThGoSa2003 <tgsml2003@gmail.com>
2022-04-29 14:35:43 +0200

    Unificaió de llenguatge

--- metro.py
+++ metro.py
@@ -7,7 +7,13 @@ from typing import Optional, List
 
 MetroGraph = networkx.Graph
 
-def get_metro_graph() -> MetroGraph: ...
+def get_metro_graph() -> MetroGraph:
+    """
+
+    """
+
+    stations = read_stations()
+    accessos =
 
 
 Position = tuple[float,float]
@@ -30,26 +36,34 @@ Accesses = list[Access]
 
 
 def read_stations() -> Stations:
-    csv_estacions = pd.read_csv('https://raw.githubusercontent.com/jordi-petit/ap2-metro-nyam-2022/main/data/estacions.csv')
-    dim = csv_estacions.shape
+    """
+
+    """
+
+    csv_stations = pd.read_csv('https://raw.githubusercontent.com/jordi-petit/ap2-metro-nyam-2022/main/data/estacions.csv')
+    dim = csv_stations.shape
     stations = []
     for i in range(dim[0]):
-        name = csv_estacions.iloc[i,7]
-        accessibility = csv_estacions.iloc[i,18] == "Accessible"
-        pos = tuple(map(float,csv_estacions.iloc[i,26][7:-1].split()))
+        name = csv_stations.iloc[i,7]
+        accessibility = csv_stations.iloc[i,18] == "Accessible"
+        pos = tuple(map(float,csv_stations.iloc[i,26][7:-1].split()))
         stations.append(Station(name,accessibility,pos))
     return stations
 
 def read_accesses() -> Accesses:
-    csv_accessos = pd.read_csv('https://raw.githubusercontent.com/jordi-petit/ap2-metro-nyam-2022/main/data/accessos.csv')
-    dim = csv_accessos.shape
-    accessos = []
+    """
+
+    """
+
+    csv_accesses = pd.read_csv('https://raw.githubusercontent.com/jordi-petit/ap2-metro-nyam-2022/main/data/accessos.csv')
+    dim = csv_accesses.shape
+    accesses = []
     for i in range(dim[0]):
-        name = csv_accessos.iloc[i,6]
-        accessibility = csv_accessos.iloc[i,8] == "Accessible"
-        pos = tuple(map(float,csv_accessos.iloc[i,-1][7:-1].split()))
-        accessos.append(Access(name, accessibility,pos))
-    return accessos
+        name = csv_accesses.iloc[i,6]
+        accessibility = csv_accesses.iloc[i,8] == "Accessible"
+        pos = tuple(map(float,csv_accesses.iloc[i,-1][7:-1].split()))
+        accesses.append(Access(name, accessibility,pos))
+    return accesses
 
 
 def show(g: MetroGraph) -> None: ...
